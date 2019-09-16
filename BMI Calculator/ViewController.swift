239d58b8839e30c80d6482d8daa0eca19d1172b9
Sacha Behrend <sacha.behrend@gmail.com>
2019-09-16 14:11:58 +0200

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Sacha Behrend on 11/09/2019.
+//  Copyright © 2019 Sacha Behrend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
